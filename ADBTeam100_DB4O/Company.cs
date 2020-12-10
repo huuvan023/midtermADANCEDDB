2ec9eacd63bb8326cb2fd752cf0e685767453356
Ngo Van <huu.van.23tg@gmail.com>
2020-12-10 08:01:40 +0700

    Add project files.

---
+++ ADBTeam100_DB4O/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2020
+{
+    [Serializable]
+    public class CongTy
+    {
+        public Guid MaCongTy { get; set; }
+		public string MaSoThue { get; set; }
+        public string TenCongTy { get; set; }
+        public string SoNha { get; set; }
+        public string DuongPho { get; set; }
+        public string QuanHuyen { get; set; }
+		public NhanVien GiamDoc { get; set; }
+
+        public override string ToString()
+        {
+            return TenCongTy;
+        }
+    }
+}
---
+++ ADBTeam100_DB4O/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2020
+{
+    [Serializable]
+    public class NhanVien
+    {
+        public Guid ID { get; set; }
+        public string HoTen { get; set; }
+        public string Skill { get; set; }
+        public CongTy HomeBase { get; set; }
+        public NhanVien QuanLy { get; set; }
+        public double Luong { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1} ({2})", HoTen, Skill, Luong);
+        }
+    }
+}
